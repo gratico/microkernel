8c67c97c3ac81443c6b71ed8d7fab997b5c0aca6
abhishiv <abhishiv@gmail.com>
2021-03-21 13:03:13 +0000

    Deploying to gh-pages from  @ 449bd5ae23c979ea03a6969b3975e11d386b1554 🚀

--- sdk/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./utils";
-export * from "./fs_helpers";
-export * from "./url_helpers";
-export * from "./git";
--- theme.ts
+++ theme.ts
@@ -1,33 +1,41 @@
-export interface ComponentStyle {
-  [key: string]: any;
-}
-export interface Theme {
-  name: string;
-  description: string;
-  url: string;
-  author: string;
-  dependencies: string[];
-  general: {
-    [key: string]: any;
-  };
-  components: {
-    [componentId: string]: ComponentStyle;
-  };
-}
-
+export type Theme = any
 export interface IComponentStyle {
-  [key: string]: any;
+  [key: string]: any
 }
 export interface ITheme {
-  name: string;
-  description: string;
-  url: string;
-  author: string;
-  dependencies: string[];
-  defs: {
-    [name: string]: any;
-  };
-  components: {
-    [name: string]: IComponentStyle;
-  };
+  meta: {
+    name: string
+    description: string
+    url: string
+    author: string
+  }
+  dependencies: Record<any, any>
+  tokens: {
+    [name: string]: any
+  }
+}
+
+interface SystemUIThemeSpecifications {
+  borders?: { [name: string]: number }
+  borderStyles?: { [name: string]: number }
+  borderWidths?: { [name: string]: number }
+  colors?: { [name: string]: string } | { modes?: { [name: string]: string } }
+  fonts?: { [name: string]: string }
+  fontSizes?: { [name: string]: number }
+  fontWeights?: { [name: string]: number }
+  letterSpacings?: { [name: string]: number }
+  lineHeights?: { [name: string]: number }
+  radii?: { [name: string]: number }
+  space?: { [name: string]: number }
+  sizes?: { [name: string]: number }
+  transitionDurations?: { [name: string]: number }
+  transitionTiming?: {
+    [name: string]: {
+      x1: number
+      y1: number
+      x2: number
+      y2: number
+    }
+  }
+  zIndices?: { [name: string]: number }
 }
